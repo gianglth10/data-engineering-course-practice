19b9efae3b6b1e160fa071a6e3de6fdb247b3efc
Giang Le (Stella) <130233775+gianglth10@users.noreply.github.com>
2026-01-26 20:24:03 +0000

    pipeline

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,11 +1,11 @@
 import sys
+import pandas as pd
+
 print("arguments", sys.argv)
 
 month = int(sys.argv[1])
 
 print(f'hello pipeline, month={month}')
-
-import pandas as pd
 
 df = pd.DataFrame({"day": [1, 2], "num_passengers": [3, 4]})
 df['month'] = month
